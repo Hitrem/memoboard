9e4d391fbf896113e41f9eaa457ade7130a6ef28
Hitrem <raudibert90@gmail.com>
2022-04-06 11:18:55 +0200

    Initial commit

---
+++ memoboard_back/src/main/java/tech/memoboard/memoboard/MemoboardApplication.java
@@ -0,0 +1,13 @@
+package tech.memoboard.memoboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoboardApplication.class, args);
+	}
+
+}
---
+++ memoboard_back/src/test/java/tech/memoboard/memoboard/MemoboardApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.memoboard.memoboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
