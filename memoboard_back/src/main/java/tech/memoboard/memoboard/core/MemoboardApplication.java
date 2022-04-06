793c490ad5c735191cab9b4e0eba55ec982eba2b
Hitrem <raudibert90@gmail.com>
2022-04-06 14:12:48 +0200

    Final BR structure

--- memoboard_back/src/main/java/tech/memoboard/memoboard/core/MemoboardApplication.java
+++ memoboard_back/src/main/java/tech/memoboard/memoboard/core/MemoboardApplication.java
@@ -1,4 +1,4 @@
-package tech.memoboard.memoboard;
+package tech.memoboard.memoboard.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
