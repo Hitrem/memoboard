4bfdf36766ef553e7f0546bb51ef8ae4cde63a13
Hitrem <raudibert90@gmail.com>
2022-04-13 15:24:32 +0200

    Token and mail confirmation

--- memoboard_back/src/main/java/com/example/memoboard/model/AppUser.java
+++ memoboard_back/src/main/java/com/example/memoboard/model/AppUser.java
@@ -30,9 +30,9 @@ public class AppUser implements UserDetails {
     )
     private Long id;
 
-    private String name;
+    private String firstName;
 
-    private String username;
+    private String lastName;
 
     private String email;
 
@@ -40,23 +40,27 @@ public class AppUser implements UserDetails {
 
     @Enumerated(EnumType.STRING)
     private AppUserRole appUserRole;
-    private Boolean locked;
-    private Boolean enabled;
 
-    public AppUser(String name, String username, String email, String password, AppUserRole appUserRole, Boolean locked, Boolean enabled) {
-        this.name = name;
-        this.username = username;
+    private Boolean locked = false;
+    private Boolean enabled = false;
+
+    public AppUser(String firstName, String lastName, String email, String password, AppUserRole appUserRole) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
         this.password = password;
         this.appUserRole = appUserRole;
-        this.locked = locked;
-        this.enabled = enabled;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         SimpleGrantedAuthority authority = new SimpleGrantedAuthority(appUserRole.name());
         return Collections.singletonList(authority);
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
     }
 
     @Override
--- memoboard_back/src/main/java/com/example/memoboard/repository/MemoRepository.java
+++ memoboard_back/src/main/java/com/example/memoboard/repository/MemoRepository.java
@@ -2,9 +2,12 @@ package com.example.memoboard.repository;
 
 import com.example.memoboard.model.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Repository
 public interface MemoRepository extends JpaRepository<Memo, Long> {
 
     List<Memo> findByNameLike(String name);
